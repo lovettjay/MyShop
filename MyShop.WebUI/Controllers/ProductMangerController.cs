313deb2370c92d7424c97f5864783b3bbb3e93f7
lovettjay <lovettjay@gmail.com>
2018-03-14 09:24:49 -0500

    Commit

---
+++ MyShop.WebUI/Controllers/ProductMangerController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyShop.Core.Models;
+using MyShop.DataAccess.InMemory;
+
+
+namespace MyShop.WebUI.Controllers
+{
+    public class ProductMangerController : Controller
+    {
+        ProductRepository context;
+
+        public ProductMangerController()
+        {
+            context = new ProductRepository();
+        }
+
+        // GET: ProductsManger
+        public ActionResult Index()
+        {
+            List<Product> products = context.Collection().ToList();
+            return View(products);
+        }
+
+        public ActionResult Create()
+        {
+            Product product = new Product();
+            return View(product);
+        }
+
+        public ActionResult Create(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(product);
+            }
+            else
+            {
+                context.Insert(product);
+                context.Commit();
+
+                return RedirectToAction("Index");
+            }
+            
+        }
+
+
+        public ActionResult Edit(string Id)
+        {
+            Product product = context.Find(Id);
+
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+
+        public ActionResult Edit(Product product, string Id)
+    }
+}
