7921a0d39505472531ce2cace046210f2d7914c6
lovettjay <lovettjay@gmail.com>
2018-03-14 10:31:52 -0500

    BaseProject

---
+++ MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyShop.WebUI.Controllers
+{
+    public class ProductManagerController : Controller
+    {
+        // GET: ProductManager
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        // GET: ProductManager
+        public ActionResult Index1()
+        {
+            return View();
+        }
+    }
+}
