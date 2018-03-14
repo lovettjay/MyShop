ad1f370061c5428245bf681496a0157b0003f91e
lovettjay <lovettjay@gmail.com>
2018-03-14 11:11:15 -0500

    Product Manager Pages Build

--- MyShop.WebUI/Controllers/ProductMangerController.cs
+++ MyShop.WebUI/Controllers/ProductMangerController.cs
@@ -31,6 +31,7 @@ namespace MyShop.WebUI.Controllers
             return View(product);
         }
 
+        [HttpPost]
         public ActionResult Create(Product product)
         {
             if (!ModelState.IsValid)
@@ -62,6 +63,65 @@ namespace MyShop.WebUI.Controllers
             }
         }
 
+        [HttpPost]
         public ActionResult Edit(Product product, string Id)
+        {
+            Product productToEdit = context.Find(Id);
+
+            if (productToEdit == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(product);
+                }
+                productToEdit.Category = product.Category;
+                productToEdit.Description = product.Description;
+                productToEdit.Image = product.Image;
+                productToEdit.Name = product.Name;
+                productToEdit.Price = product.Price;
+
+                context.Commit();
+
+                return RedirectToAction("Index");
+            }
+
+        }
+
+        public ActionResult Delete(string Id)
+        {
+            Product productToDelete = context.Find(Id);
+
+            if (productToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(productToDelete);
+            }
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string Id)
+        {
+            Product productToDelete = context.Find(Id);
+
+            if (productToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                context.Delete(Id);
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+
     }
 }
